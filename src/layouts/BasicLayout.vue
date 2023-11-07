610da839b360e4b7ae57e8f884b0dd5872ae8b6f
一只枫兔 <114807153+Schuke-Shu@users.noreply.github.com>
2023-11-07 08:37:09 +0800

    2023/11/07

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px;">
+      <a-layout-header class="header">导航栏</a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="http://mabbit.cn" target="_blank">兔子洞</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style>
+  #basicLayout .header
+  {
+      background-color: red;
+      margin-bottom: 16px;
+  }
+
+  #basicLayout .content
+  {
+      background: linear-gradient(to right, #bbb, #fff);
+      margin-bottom: 16px;
+  }
+
+  #basicLayout .footer
+  {
+      background-color: #efefef;
+      padding: 16px;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      text-align: center;
+  }
+</style>
